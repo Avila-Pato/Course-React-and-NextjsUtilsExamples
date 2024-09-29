32758d763398f66dafd85321fab28cdd819ff79c
Patricio Avila <p.avil;af1998@gmail.com>
2024-09-29 16:30:54 -0300

    Agregando datatable+materialMui

---
+++ TableReactNextjs/react-data-table-example/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import Table from './components/Table'
+
+function App() {
+
+  return (
+    <>
+    <Table/>
+    </>
+  )
+}
+
+export default App
